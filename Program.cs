e6ae79d1e07aafb4261a4ca1a895be20efa685f9
Camilla <camilla.e.soderman@gmail.com>
2024-10-30 11:13:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chas_Ching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
