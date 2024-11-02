4e0999d85d01c376d103042b30afef40569f8bd9
jyneto <j-pm@live.se>
2024-11-02 20:09:26 +0100

    Första draft. Deklarerat Accounts som en lista av typen Account OpenAccount flyttat hit ViewAccount skapades

--- Core/Models/Customer.cs
+++ Core/Models/Customer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,47 +10,79 @@ namespace Chas_Ching.Core.Models
 {
     public class Customer : User
     {
-        // Deklarera Accounts som en lista av typen Account
+        //KRAV: Som användare vill jag kunna se en lista med alla mina bankkonton och saldot på dessa.
+
+        // Declare Accounts as a list of type Account
         public List<Account> Accounts { get; set; }
         public decimal Loan { get; set; }
 
-        public Customer(string userName, string password) : base(userName, password)
+        //Constructor
+        public Customer(string userEmail, string password) : base(userEmail, password)
         {
-            // Initiera Accounts-listan
+            // Initialize Accounts-list
             Accounts = new List<Account>();
             Loan = Loan;
         }
 
-        public override void Login()
-        {
-            throw new NotImplementedException();
-        }
 
-        public override void Logout()
-        {
-            throw new NotImplementedException();
-        }
-        public void ViewAccounts()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void TransferFounds()
-        {
-            throw new NotImplementedException();
-        }
         public void OpenAccount()
         {
+            Console.WriteLine("Enter the initial balance for the new account: ");
+            decimal initialBalamce;
+
+            while (!decimal.TryParse(Console.ReadLine(), out initialBalamce) || initialBalamce < 0)
+            {
+                Console.WriteLine("Invalid inpuut. Plaese enter a non-negativ number: ");
+            }
+
+            var newAccount = new Account(Accounts.Count + 1, initialBalamce);
+            Accounts.Add(newAccount);
+            Console.WriteLine¤($"Acount created with ID {newAccount.ID} and balance {newAccount.Balance}");
+        }
+
+        //Method for viewing accounts
+        public void ViewAccounts()
+        {
+            Console.WriteLine("Viewing accounts...");
+
+            if (Accounts.Count == 0)
+            {
+                Console.WriteLine("No accounts found");
+            }
+            else
+            {
+                foreach (var account in Accounts)
+                    Console.WriteLine($"Account ID: {account.ID}, Balance: {account.Balance}");
+
+            }
+            public void TransferFounds()
+        {
             throw new NotImplementedException();
         }
+
 
         public void RequestLoan()
         {
             throw new NotImplementedException();
         }
+
+
         public void ViewTransaction()
         {
             throw new NotImplementedException();
+        }
+    }
+    // Kanske en bra ide att använda Guid för ID?
+    public class Account
+    {
+        public ID { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account()
+        {
+           ID = id;
+           Balance = Balance;
+
         }
     }
 }
