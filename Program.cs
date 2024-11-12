23669b80744c878a201c0dea1a7584a9cfd322cc
mao <mowitz.almstedt@gmail.com>
2024-11-12 15:07:00 +0100

    Removed temporary customer for debugging

--- Program.cs
+++ Program.cs
@@ -6,9 +6,6 @@ namespace Chas_Ching
     {
         public static void Main()
         {
-            User user1 = new Customer("a@a.se", "Mao123!"); // Temporary user for testing
-            UserManagement.RegisterUser(user1.UserEmail, user1.Password); // Register user
-                
             var mainMenu = new MainMenu();
             mainMenu.Start();
         }
