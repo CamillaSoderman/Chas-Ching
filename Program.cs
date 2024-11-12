48d15ece0b3b06dd83ca9f475ab6ba389ffa59b6
mao <mowitz.almstedt@gmail.com>
2024-11-12 13:43:03 +0100

    Added test user for debugging

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Chas_Ching
+﻿using Chas_Ching.Core.Models;
+
+namespace Chas_Ching
 {
     public class Program
     {
         public static void Main()
         {
+            User user1 = new Customer("a@a.se", "Mao123!"); // Temporary user for testing
+            UserManagement.RegisterUser(user1.UserEmail, user1.Password); // Register user
+                
             var mainMenu = new MainMenu();
             mainMenu.Start();
         }
